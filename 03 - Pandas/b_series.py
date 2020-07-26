2e4a5e9a068e98ebc14cae544875bcfc9a18048d
Narváez Jeferson <alexresidentevil@gmail.com>
2020-07-26 17:14:46 -0500

    Pandas y artwork

--- 03 - Pandas/b_series.py
+++ 03 - Pandas/b_series.py
@@ -40,3 +40,36 @@ s5=serie_valor_ciudad[ciudades_menor_5k]
 serie_valor_ciudad=serie_valor_ciudad*1.1
 serie_valor_ciudad["Quito"]=serie_valor_ciudad["Quito"]-50
 print ("Lima" in serie_valor_ciudad)
+
+
+
+
+
+svc_cuadrado=np.square(serie_valor_ciudad)
+ciudades_uno=pd.Series({"Montañita":3000,
+                        "Guayaquil":10000,
+                        "Quito":2000})
+
+ciudades_dos=pd.Series({"Loja":3000,
+                        "Ambato":10000})
+ciudades_uno["Loja"]=0
+print(ciudades_uno+ciudades_dos)
+#ademas de add hay sub mul div
+ciudaddes_add=ciudades_uno.add(ciudades_dos)
+ 
+ #El verify_integrity sirve para ver si los datos se estan repitiendo
+#concat y append es lo mismo
+#ciudad_concat=pd.concat([ciudades_uno,ciudades_dos],verify_integrity=True)
+print(ciudades_uno.max())
+print(ciudades_uno.min())
+
+print(ciudades_uno.mean())
+print(ciudades_uno.median())
+print(np.average(ciudades_uno))
+print(ciudades_uno.head(2))
+print(ciudades_uno.tail(2))
+resultado=ciudade_uno.where(ciudades_uno<5999)
+
+
+
+
